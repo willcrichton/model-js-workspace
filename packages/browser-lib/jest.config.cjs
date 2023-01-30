cc30e6ce0e90d797e1afb4dba509f8f9d13738bc
Will Crichton <wcrichto@cs.stanford.edu>
2023-01-29 20:47:57 -0800

    Add jest tests

---
+++ packages/browser-lib/jest.config.cjs
@@ -0,0 +1,6 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: "ts-jest/presets/js-with-ts-esm",
+  testEnvironment: "jsdom",
+  roots: ['<rootDir>/tests']
+};
---
+++ packages/browser-lib/tests/lib.test.ts
@@ -0,0 +1,3 @@
+import { add } from "browser-lib";
+
+test("add", () => expect(add(1, 2)).toBe(3));
