fed00529bad72f1e7e2c68c6d809b1687f1ecd3f
Will Crichton <wcrichto@cs.stanford.edu>
2023-01-29 17:46:13 -0800

    Add file import

--- packages/browser-lib/src/Globals.d.ts
+++ packages/browser-lib/src/Globals.d.ts
@@ -1,1 +1,9 @@
-declare module "*.module.css";
+declare module "*?file" {
+  const url: string;
+  export default url;
+}
+
+declare module "*.module.css" {
+  const classes: {[cls: string]: string};
+  export default classes;
+}
--- packages/browser-lib/src/components/button.tsx
+++ packages/browser-lib/src/components/button.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import classes from "./button.module.css";
+import icon from "./icon.jpg?file";
 
 /** A very special button */
 export let Button: React.FC<React.PropsWithChildren> = ({ children }) => (
-  <button className={classes["special-button"]}>{children}</button>
+  <button className={classes["special-button"]}>
+    {children} <img src={icon} />
+  </button>
 );
